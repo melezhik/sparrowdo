cad5e36d9272cc70ff7a6f0aad0a05ce68d0e4a7
Alexey Melezhik <melezhik@gmail.com>
2020-02-05 23:25:56 +0000

    we don't need travis

---
+++ rakudist-run.sh
@@ -0,0 +1,15 @@
+token=$(curl -d os=alpine -d project=melezhik/sparrowdo http://repo.westus.cloudapp.azure.com/rakudist/api/run/:github)
+while true; do
+  status=$(curl -s -d token=$token http://repo.westus.cloudapp.azure.com/rakudist/api/job/status)
+  sleep 5
+  echo $status
+  if [ $status != "running" ]; then
+    break
+  fi
+done
+echo "test: $status"
+curl -L -s -d token=$token http://repo.westus.cloudapp.azure.com/rakudist/api/job/report
+if [ $status != "success" ]; then
+  exit 1
+fi
+
