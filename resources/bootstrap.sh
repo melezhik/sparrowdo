2d3e165d8fbbad8d5b25effad523f4f098cffa3d
Alexey Melezhik <melezhik@gmail.com>
2025-04-25 12:02:33 +0300

    use Use native Alpine packages for bootstrap

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -40,71 +40,75 @@ install_zef()
   fi
 }
 
+install_sparrow()
+{
+
+  if raku -MSparrow6 -e 1 2>/dev/null; then
+    echo "Sparrow6 already installed"
+  else
+    zef install --/test OpenSSL
+    zef install --/test JSON::Tiny
+    zef install --/test IO::Socket::SSL
+    zef install --/test HTTP::Tiny
+    zef install --/test MIME::Base64
+    zef install --/test File::Directory::Tree
+    zef install --/test Data::Dump
+    zef install --/test JSON::Fast
+    zef install --/test Terminal::ANSIColor
+    zef install --/test YAMLish
+    zef install --/test Sparrow6
+  fi
+
+  zef upgrade --/test Sparrow6
+
+  if raku -MSparky::JobApi -e 1 2>/dev/null; then
+    echo "Sparky::JobApi already installed"
+  else
+    zef install --/test  Sparky::JobApi
+  fi
+
+}
+
+# =============================================================================
 
 case "$OS" in
   alpine)
+    repo=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
     apk update --wait 120
-    apk add --no-cache --wait 120 curl perl bash git openssl-dev
-    export PATH=/opt/rakudo-pkg/bin/:/opt/rakudo-pkg/share/perl6/site/bin:$PATH
-    if raku --version 2>/dev/null; then
-      echo "rakudo already installed"
-    else
-      curl -1sLf 'https://dl.cloudsmith.io/public/nxadm-pkgs/rakudo-pkg/setup.alpine.sh' | bash
-      apk add rakudo-pkg
-    fi
-    install_zef
+    apk add --no-cache --wait 120 curl bash zef
+    apk add -u --repository=$repo \
+    raku-sparrow6 raku-sparky-job-api
   ;;
   amazon|centos|red|rocky)
     yum -q -y install curl bash wget openssl-devel perl-JSON-PP
     install_rakudo_linux
+    install_sparrow
   ;;
   arch|archlinux)
     pacman -Sy --noconfirm archlinux-keyring
     pacman -Suyy --noconfirm
     pacman -S --needed --noconfirm -q curl bash openssl openssl-1.1
     install_rakudo_linux
+    install_sparrow
   ;;
   debian|ubuntu)
     DEBIAN_FRONTEND=noninteractive
     apt-get update -q -o Dpkg::Use-Pty=0
     apt-get install -q -y -o Dpkg::Use-Pty=0 curl bash libssl-dev
     install_rakudo_linux
+    install_sparrow
   ;;
   fedora)
     dnf -yq install curl bash openssl-devel
     install_rakudo_linux
+    install_sparrow
   ;;
   opensuse)
     zypper install -y curl tar gzip libopenssl-devel
     install_rakudo_linux
+    install_sparrow
   ;;
   *)
     printf "Your OS (%s) is not supported\n" "$OS"
     exit 1
 esac
-
-if raku -MSparrow6 -e 1 2>/dev/null; then
-  echo "Sparrow6 already installed"
-else
-  zef install --/test OpenSSL
-  zef install --/test JSON::Tiny
-  zef install --/test IO::Socket::SSL
-  zef install --/test HTTP::Tiny
-  zef install --/test MIME::Base64
-  zef install --/test File::Directory::Tree
-  zef install --/test Data::Dump
-  zef install --/test JSON::Fast
-  zef install --/test Terminal::ANSIColor
-  zef install --/test YAMLish
-  zef install --/test Sparrow6
-fi
-
-zef upgrade --/test Sparrow6
-
-if raku -MSparky::JobApi -e 1 2>/dev/null; then
-  echo "Sparky::JobApi already installed"
-else
-  zef install --/test  Sparky::JobApi
-fi
-
-
