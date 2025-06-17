15cf67376fb65d313f3aa68e71a1a920cce58635
Alexey Melezhik <melezhik@gmail.com>
2025-06-17 16:35:27 +0300

    workaround for rhel/curl issue

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -67,13 +67,13 @@ case "$OS" in
     echo "install Sparrow6, Sparky-Job-Api ..."
     apk add --no-cache --wait 120 -u --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/ raku-sparrow6 raku-sparky-job-api
   ;;
-  rocky)
+  rocky|rhel)
     yum -q -y install curl-minimal || yum -q -y install curl
     yum -q -y install bash wget openssl-devel perl-JSON-PP
     install_rakudo_linux
     install_sparrow
   ;;
-  amazon|centos|red|fedora|rhel)
+  amazon|centos|red|fedora)
     yum -q -y install curl bash wget openssl-devel perl-JSON-PP
     install_rakudo_linux
     install_sparrow
