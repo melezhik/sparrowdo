9bb5fc163e169a5e0f6b1ead54d65c5513c72419
sp1983 <kibijel@gmail.com>
2026-04-08 16:18:29 +0000

    raku-sparky-job-api is ready

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -72,13 +72,8 @@ case "$OS" in
       source /etc/os-release
       if [[ "$VERSION_ID" == 9* ]]; then
         dnf copr enable grayeul/TestProj -y
-        yum install raku-sparrow6 -y -q
-        export PATH=/opt/rakudo/bin:/opt/rakudo/share/perl6/site/bin:~/.raku/bin:$PATH
-        yum install git -y -q
-        git clone https://github.com/ugexe/zef.git
-        cd zef
-        raku -I. bin/zef install . --/test
-        zef install --/test Sparky::JobApi
+        yum install raku-sparrow6 raku-sparky-job-api -y -q
+        export PATH=/opt/rakudo/bin:/opt/rakudo/share/perl6/site/bin:$PATH
       else
         install_rakudo_linux
         install_sparrow
